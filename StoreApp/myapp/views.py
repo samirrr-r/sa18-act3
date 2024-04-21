1732b8e021149d2e7a51ce89fea533388d873caf
Samir Abdosh <samirabdosh1@gmail.com>
2024-04-20 20:43:55 -0500

    Made the controller, views, and pages. Fixed the DB issues

--- StoreApp/myapp/views.py
+++ StoreApp/myapp/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Product
 
 # Create your views here.
+
+def index(request):
+    products = Product.objects.all()
+    return render(request, "products/index.html", {"products": products})
+    
+def show(request, id):
+    product = get_object_or_404(Product, pk=id)
+    return render(request, 'products/show.html', {'product': product})

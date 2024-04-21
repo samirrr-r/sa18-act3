fe64693db769c9cf76e9e22adb685ead52a48eec
Samir Abdosh <samirabdosh1@gmail.com>
2024-04-20 19:19:10 -0500

    Changes db  to postgre and made the models

--- StoreApp/StoreApp/settings.py
+++ StoreApp/StoreApp/settings.py
@@ -74,10 +74,14 @@ WSGI_APPLICATION = 'StoreApp.wsgi.application'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+'default': {
+'ENGINE': 'django.db.backends.postgresql',
+'NAME': 'mydatabase',
+'USER': 'mydatabaseuser',
+'PASSWORD': 'mypassword',
+'HOST': 'localhost',
+'PORT': '',
+}
 }
 
 
--- StoreApp/myapp/models.py
+++ StoreApp/myapp/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+
+class Product(models.Model):
+    name = models.CharField(max_length=255)
+    price = models.DecimalField(decimal_places=2)
+    description = models.TextField()
